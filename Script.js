6cfe2cd71a0ad72663010c3280298b09776fbaa9
MubarakGj <g.mubarak06@gmail.com>
2023-04-09 02:41:56 +0530

    display score and ultimate winner feature is added

--- Script.js
+++ Script.js
@@ -1,4 +1,6 @@
 let options = ["ROCK", "PAPER", "SCISSORS"];
+let playerWinCount = 0;
+let computerWinCount = 0;
 function getPlayerChoice(e) {
     return e.target.dataset.value;
 }
@@ -20,11 +22,13 @@ function playRound(playerChoice, computerChoice) {
         case (playerChoice === "ROCK" && computerChoice === "SCISSORS"):
         case (playerChoice === "PAPER" && computerChoice === "ROCK"):
             message = `You Win!! ${playerChoice} beats ${computerChoice}`;
+            playerWinCount++;
             break;
         case (playerChoice === "PAPER" && computerChoice === "SCISSORS"):
         case (playerChoice === "SCISSORS" && computerChoice === "ROCK"):
         case (playerChoice === "ROCK" && computerChoice === "PAPER"):
             message = `You Lose!! ${computerChoice} beats ${playerChoice}`;
+            computerWinCount++;
             break;
         case (playerChoice === computerChoice):
             message = "Its a Tie!! Try again";
@@ -38,8 +42,22 @@ function game(e) {
         let playerChoice = getPlayerChoice(e);
         let computerChoice = getComputerChoice();
         let result = document.querySelector("#result");
-        result.innerHTML = playRound(playerChoice, computerChoice);
+        let message = playRound(playerChoice, computerChoice);
+        playerScore.textContent = playerWinCount;
+        computerScore.textContent = computerWinCount;
+        result.innerHTML =  message;
+
+        if(playerWinCount === 5 || computerWinCount === 5){
+            let msg = "First one to win 5 consecutive games is: "
+            result.innerHTML = (playerWinCount === 5) ? msg.concat("Player") : msg.concat("Computer");
+            playerWinCount = 0;
+            computerWinCount = 0;
+
+        }
 }
+
+let playerScore = document.querySelector("#playerScore");
+let computerScore = document.querySelector("#computerScore");
 
 const buttons = document.querySelectorAll("button");
 buttons.forEach(button => {
